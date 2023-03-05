3813433573443ed54d0aa1e4491aa7eea49a9566
simonp20 <parker_simon@brown.edu>
2023-03-05 22:50:14 +1300

    automatically generated commit message

--- chip_8/main.cpp
+++ chip_8/main.cpp
@@ -1,5 +1,17 @@
 #include "main.hpp"
+#include "memory.hpp"
 
 int main() {
     printf("hello, world\n");
+    ram someRam;
+    reg regs;
+    someRam.mem[0] = 0;
+    if(someRam.mem[0] == 0){
+        printf("howdy world!!\n");
+    }
+    regs.v0 = 1;
+    if(regs.v0 == 1) {
+        printf("yep\n");
+    }
 }
+
---
+++ chip_8/memory.hpp
@@ -0,0 +1,22 @@
+struct ram {
+    char mem[4096];
+};
+
+struct reg {
+    char v0;
+    char v1;
+    char v2;
+    char v3;
+    char v4;
+    char v5;
+    char v6;
+    char v7;
+    char v8;
+    char v9;
+    char va;
+    char vb;
+    char vc;
+    char vd;
+    char ve;
+    char vf;
+};
